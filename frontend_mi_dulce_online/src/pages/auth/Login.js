9484b63ac9512ad62e22b1d3e827e60d32ccd6d3
Swiggui <figueredojuan2020@outlook.es>
2022-11-13 20:32:13 -0500

    UM1-26 Formulario Login Completado

--- frontend_mi_dulce_online/src/pages/auth/Login.js
+++ frontend_mi_dulce_online/src/pages/auth/Login.js
@@ -2,7 +2,23 @@ import React from "react";
 
 const Login = () => {
     return (
-        <div><h1>LOGIN</h1></div>
+        <div className="container-xl position-absolute top-50 start-50 translate-middle">
+            <h1 className="text-center mb-3">Iniciar Sesión</h1>
+            <div className="container">
+                <form>
+                  <div class="form-floating mb-3">
+                    <input type="email" class="form-control" id="floatingInput" placeholder="Usuario"/>
+                    <label for="floatingInput">Usuario</label>
+                    </div>                  
+                  <div class="form-floating mb-3">
+                  <input type="password" class="form-control" id="floatingPassword" placeholder="Contraseña"/>
+                    <label for="floatingPassword">Contraseña</label>
+                  </div>
+                  <button type="submit" class="btn btn-outline-primary w-100 my-2">Iniciar Sesión</button>
+                  <button className="btn btn-outline-success w-100 my-2">Crear Cuenta</button>
+                </form>
+            </div>
+        </div>
     );
 }
 
