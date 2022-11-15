14176409f3a854d6b936edf480633e740d174a7e
Swiggui <figueredojuan2020@outlook.es>
2022-11-14 20:01:50 -0500

    Navbar completo

--- frontend_mi_dulce_online/src/components/Navbar.js
+++ frontend_mi_dulce_online/src/components/Navbar.js
@@ -11,28 +11,23 @@ const Nabvar = () => {
         <div>
             <nav className="navbar navbar-expand-lg">
               <div className="container-fluid">
-                <Link to={"/Home"} className="navbar-brand" href="#">
-                  <img src="/favicon.png" alt="Logo" width="30" height="30" className="d-inline-block align-text-top me-2" />
-                  Mi Dulce Online
-                </Link>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                  <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li className="nav-item d-flex">
-                      <a className="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li className="nav-item d-flex">
-                      <a className="nav-link" href="#">Features</a>
-                    </li>
-                    <li className="nav-item d-flex">
-                      <a className="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li className="nav-item d-flex">
-                      <a className="nav-link disabled">Disabled</a>
-                    </li>
-                  </ul>
+              <Link to={"/Home"} className="navbar-brand" href="#">
+               <img src="/favicon.png" alt="Logo" width="30" height="30" className="d-inline-block align-text-top me-2" />
+               Mi Dulce Online
+             </Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon" />
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                  <div className="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <a className="nav-link mx-1" href="#">Listar</a>
+                    <a className="nav-link mx-1" href="#">Crear</a>
+                    <button  onClick={onClick} className="btn btn-outline-danger mx-2" type="button">Salir</button>
+                  </div>
                 </div>
               </div>
             </nav>
+            
         </div>
     );
 }
